e65074f49e1f76eeed3fb81754fdc1a600bf10fd
Michael Vasios <mvasios1@gmail.com>
2017-12-03 19:22:50 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -4,7 +4,7 @@ function forLoop(arr) {
       console.log("I am ${i} strange loop")
       }
     else {
-    console.log("I am ${i} strange loops")
+      console.log("I am ${i} strange loops")
       }
   }
 }
